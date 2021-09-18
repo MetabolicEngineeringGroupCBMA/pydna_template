c77ea1709b63538f3de4faa9006152fd6eac65b9
BjornFJohansson <bjornjobb@gmail.com>
2021-09-18 15:24:46 +0100

    fix

---
+++ accessory/nbcollector.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+
+links = [f"- [{p.stem}]({p.relative_to(Path.cwd())})" for p in Path.cwd().rglob("*.ipynb") if ".ipynb_checkpoints" not in p.parts]
+
+for link in sorted(links):
+    print(link)
+
+if __name__ == "__main__":
+    input("press enter to close")
