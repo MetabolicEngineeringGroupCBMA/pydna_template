913891e372b5726db3be0f6ab7268505ce4a54fa
BjornFJohansson <bjornjobb@gmail.com>
2022-02-22 06:33:42 +0000

    tqdm_notebook

---
+++ notebooks/joblib_memory_cachedir/joblib/__main__--home-bjorn-files-MEC@CBMA_genetic_constructs-pydna_template-notebooks-<ipython-input-5adaef9c72be>/slow_function/func_code.py
@@ -0,0 +1,7 @@
+# first line: 1
+@memory.cache
+def slow_function(x):
+    for i in trange(x, desc='1st loop'):
+        for j in tqdm_notebook(range(100), desc='2nd loop'):
+            sleep(0.001)
+    return "done!"
